3b0d2d97dd44fdf7789dbb98f1cb83471da1540f
DanipunK1 <pegate.eltrueco@gmail.com>
2021-05-23 02:18:28 +0200

    gapped build

--- cherish_raphael.mk
+++ cherish_raphael.mk
@@ -19,7 +19,11 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Gapps
-CHERISH_NONGAPPS=true
+CHERISH_WITHGAPPS := true
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Face Unlock Flag
 TARGET_FACE_UNLOCK_SUPPORTED := true
