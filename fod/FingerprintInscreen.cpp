dac4ab92ae342a7e883c053127254e580d282b55
DanipunK1 <pegate.eltrueco@gmail.com>
2021-07-18 03:08:22 +0200

    Revert "raphael: fod: Remove dimlayer"
    This reverts commit 743fda954ce570d9c101f3e5fdceb80171537a9e.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -18,8 +18,10 @@
 
 #include "FingerprintInscreen.h"
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
 
+#include <chrono>
 #include <cmath>
 #include <fstream>
 #include <thread>
@@ -39,11 +41,16 @@
 #define FOD_STATUS_OFF 0
 
 #define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+#define DIM_LAYER_OFF_DELAY 85ms
 
 #define FOD_SENSOR_X 445
 #define FOD_SENSOR_Y 1931
 #define FOD_SENSOR_SIZE 190
 
+#define DIM_LAYER_HBM_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_hbm"
+
+using ::android::base::WriteStringToFile;
+using namespace std::chrono_literals;
 
 namespace {
 
@@ -72,6 +79,10 @@ static bool readBool(int fd) {
     return c != '0';
 }
 
+// Write value to path and close file.
+bool WriteToFile(const std::string& path, uint32_t content) {
+    return WriteStringToFile(std::to_string(content), path);
+}
 }  // anonymous namespace
 namespace vendor {
 namespace lineage {
@@ -131,6 +142,7 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    WriteToFile(DIM_LAYER_HBM_PATH, 1);
     return Void();
 }
 
@@ -140,11 +152,14 @@ Return<void> FingerprintInscreen::onRelease() {
 
 Return<void> FingerprintInscreen::onShowFODView() {
     set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    WriteToFile(DIM_LAYER_HBM_PATH, 1);
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
     set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+    std::this_thread::sleep_for(DIM_LAYER_OFF_DELAY);
+    WriteToFile(DIM_LAYER_HBM_PATH, 0);
     return Void();
 }
 
