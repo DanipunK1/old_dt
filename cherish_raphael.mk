ed0270cc4a47873ff518c2b72e3c9cb30cd92b1b
DanipunK1 <pegate.eltrueco@gmail.com>
2021-08-11 14:47:53 +0200

    Revert "gapps build, without oplauncher"
    This reverts commit db68a47fdd59fde2e315d6ec9510f7a5ddd35836.

--- cherish_raphael.mk
+++ cherish_raphael.mk
@@ -18,12 +18,8 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 # Inherit some common CHERISH stuff.
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-# Gapps
-WITH_GMS = true
-TARGET_INCLUDE_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+# OPLauncher
+BUILD_WITH_OPLAUNCHER := true
 
 # Face Unlock Flag
 TARGET_FACE_UNLOCK_SUPPORTED := true
