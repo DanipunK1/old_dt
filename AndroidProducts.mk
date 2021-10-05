4a54980f107e75a60ee4b09b68a820bf9c472a5c
DanipunK1 <pegate.eltrueco@gmail.com>
2021-10-05 23:30:55 +0200

    Colt-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_raphael.mk
+    $(LOCAL_DIR)/colt_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_raphael-user \
-    spark_raphael-userdebug \
-    spark_raphael-eng
+    colt_raphael-user \
+    colt_raphael-userdebug \
+    colt_raphael-eng
--- colt_raphael.mk
+++ colt_raphael.mk
@@ -16,10 +16,10 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_raphael
+PRODUCT_NAME := colt_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9T Pro
@@ -30,8 +30,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# SparkOS stuff
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
-WITH_FOD_ANIMATIONS := true
-SPARK_BUILD_TYPE := Official
+# ColtOS stuff
+COLT_BUILD_MAINTAINER := DanipunK1
+
