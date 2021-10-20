47c22ccad5a22ccb671c960a31408f49be81888a
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-10-20 10:29:45 +0530

    Revert "raphael: livedisplay: Uprev to 2.1"
    This reverts commit fe79a1bf4491e4301494eecf83da97d65d705b8a.

--- device.mk
+++ device.mk
@@ -292,7 +292,7 @@ PRODUCT_PACKAGES += \
 # Livedisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.1-service.raphael
+    vendor.lineage.livedisplay@2.0-service.raphael
 
 # Media
 PRODUCT_PACKAGES += \
