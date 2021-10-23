237fa16bc545b591e5275c81e9e98ede3841f3b1
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-10-23 12:15:34 +0530

    Revert "[DNM] raphael: Disable IFAA/WfdCommon boot jars"
    This reverts commit fbe9835e0cb4159811880eb0129523ffaac1af0f.

--- device.mk
+++ device.mk
@@ -243,8 +243,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
 
 # Init
 PRODUCT_PACKAGES += \
@@ -539,8 +539,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-#PRODUCT_BOOT_JARS += \
-#    WfdCommon
+PRODUCT_BOOT_JARS += \
+    WfdCommon
 
 # XiaomiParts
 PRODUCT_PACKAGES += \
